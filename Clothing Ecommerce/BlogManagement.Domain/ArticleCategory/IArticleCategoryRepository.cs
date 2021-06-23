d381d8e4dc655613160bc0ea433a4656c093945c
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-06-23 23:08:44 +0430

    ArticleCategoryManagement In Blog Implemented

---
+++ Clothing Ecommerce/BlogManagement.Domain/ArticleCategory/IArticleCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using BlogManagement.Application.Contracts.ArticleCategory;
+
+namespace BlogManagement.Domain.ArticleCategory
+{
+    public interface IArticleCategoryRepository : IRepository<long, ArticleCategory>
+    {
+        EditArticleCategory GetDetails(long id);
+        List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
+    }
+}
---
+++ Clothing Ecommerce/BlogMangement.Infrastructure.EfCore/Repository/ArticleCategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Infrastructure;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategory;
+
+namespace BlogManagement.Infrastructure.EfCore.Repository
+{
+    public class ArticleCategoryRepository : RepositoryBase<long, ArticleCategory>, IArticleCategoryRepository
+    {
+        private readonly BlogContext _context;
+
+        public ArticleCategoryRepository(BlogContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditArticleCategory GetDetails(long id)
+        {
+            return _context.ArticleCategories.Select(x => new EditArticleCategory
+            {
+                CanonicalAddress = x.CanonicalAddress,
+                Description = x.Description,
+                Id = x.Id,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Name = x.Name,
+                Slug = x.Slug,
+                ShowOrder = x.ShowOrder,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
+        {
+            var query = _context.ArticleCategories.Select(x => new ArticleCategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Picture = x.Picture,
+                ShowOrder = x.ShowOrder,
+                Description = x.Description
+            });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            return query.OrderByDescending(x => x.ShowOrder).ToList();
+        }
+    }
+}
